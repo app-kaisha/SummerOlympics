fdcacfadca93f9085917e2dcef21c3a591ad80f7
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-03 12:10:50 +0000

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,18 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by app-kaihatsusha on 03/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
